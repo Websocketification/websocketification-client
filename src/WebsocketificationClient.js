4529854a08d6f6d414cdf56e0ad9614beded49d2
Fisher <fisher.subs@gmail.com>
2017-04-14 18:39:01 +0800

    Added on`Events`Listeners: onBroadcast, onHandledResponse, onClosed, and onError.

--- src/WebsocketificationClient.js
+++ src/WebsocketificationClient.js
@@ -9,9 +9,20 @@ const WS_STATUS_CONNECTED = 'CONNECTED';
 const WS_STATUS_DISCONNECTED = 'DISCONNECTED';
 const WS_STATUS_ERROR = 'ERROR';
 
+const METHOD_BROADCAST = '$BROADCAST';
+const METHOD_DEFAULT = 'GET';
+
+const getID = (method = METHOD_DEFAULT, path) => `${METHOD_BROADCAST}:${path}`;
+
 class WebsocketificationClient {
 	constructor(address) {
-		['connect', 'onResponse', 'onError', 'onClose', 'fetch', 'close'].map(method => this[method] = this[method].bind(this));
+		[
+			'connect', 'close',
+			'onResponse',
+			'fetch',
+			'setOnBroadcastListener', 'setOnUnhandledResponseListener',
+			'setOnClosedListener', 'setOnErrorListener'
+		].map(method => this[method] = this[method].bind(this));
 		this.fetch = this.fetch.bind(this);
 		this.mStatus = WS_STATUS_CONNECTING;
 		this.mAddress = address;
@@ -20,9 +31,13 @@ class WebsocketificationClient {
 		 */
 		this.mTempListeners = [];
 		/**
-		 * Temp listener that will be triggered only for once.
+		 * Global listener that will be triggered more than once.
 		 */
 		this.mGlobalListeners = [];
+		/**
+		 * Unhandled listener that will be called for unhandled response.
+		 */
+		this.mUnhandledListener = undefined;
 	}
 
 	connect() {
@@ -31,40 +46,37 @@ class WebsocketificationClient {
 			this.onResponse(JSON.parse(message.data));
 		};
 		return new Promise((resolve, reject) => {
-			let called = false;
 			this.mWS.onopen = () => {
 				this.mStatus = WS_STATUS_CONNECTED;
-				if (!called) {
-					called = true;
-					resolve();
-				}
+				resolve();
 			};
 			this.mWS.onerror = error => {
 				this.mStatus = WS_STATUS_ERROR;
-				this.onError(error);
-				if (!called) {
-					called = true;
-					reject();
-				}
+				if (this.mOnError) {this.mOnError(error);}
+				reject();
 			};
-			this.mWS.onclose = error => {
+			this.mWS.onclose = () => {
 				this.mStatus = WS_STATUS_DISCONNECTED;
-				this.onClose(error);
-				if (!called) {
-					called = true;
-					reject();
-				}
+				if (this.mOnClose) {this.mOnClose();}
+				reject();
 			};
 		})
+	}
+
+	/**
+	 * Close connection.
+	 */
+	close() {
+		this.mWS.close();
 	}
 
 	/**
 	 * Handle response.
 	 */
 	onResponse(response) {
-		if (!response || !response.method || !response.status) {return}
+		if (!response || !response.method || !response.status) {return;}
 		response.json = () => response.body;
-		const key = `${response.method || 'GET'}:${response.path}`;
+		const key = getID(response.method, response.path);
 		let listener = this.mTempListeners[key];
 		if (listener) {
 			delete this.mTempListeners[key];
@@ -78,26 +90,19 @@ class WebsocketificationClient {
 		listener = this.mGlobalListeners[key];
 		if (listener) {
 			if (200 === response.status) {
-				listener.resolve(response);
+				listener(null, response);
 			} else {
-				listener.reject(response);
+				listener(response);
 			}
 			return;
 		}
-		console.warn('UNHANDLED RESPONSE: ', key, response);
-	}
-
-	onError(error) {
-		console.log('ERROR: ', error);
-	}
-
-	onClose() {
-		// TODO ADD RETRY ON DISCONNECTED.
-		console.log('Connection Closed.');
+		if (this.mUnhandledListener) {
+			this.mUnhandledListener(response);
+		}
 	}
 
 	/**
-	 * Fetch resources.
+	 * Fetch resources like HTTP request.
 	 *
 	 * @param path Resource path.
 	 * @param options
@@ -105,21 +110,22 @@ class WebsocketificationClient {
 	 */
 	fetch(path, options = {}) {
 		options.path = path;
-		if (!options.method) {options.method = 'GET';}
+		if (!options.method) {options.method = METHOD_DEFAULT;}
 		return new Promise((resolve, reject) => {
+			let time = 100;
 			let checkConnection = () => {
 				switch (this.mStatus) {
 					case WS_STATUS_CONNECTING:
-						console.log('Waiting for WebSocket connection!');
 						setTimeout(() => {
 							checkConnection();
-						}, 100);
+						}, time);
+						// Slow down the loop.
+						time += 20;
 						break;
 					case WS_STATUS_CONNECTED:
 						this.mWS.send(JSON.stringify(options));
-						// Default send 'GET' request.
 						// Set listener.
-						this.mTempListeners[`${options.method}:${options.path}`] = {resolve, reject};
+						this.mTempListeners[getID(options.method, options.path)] = {resolve, reject};
 						break;
 					case WS_STATUS_DISCONNECTED:
 					case WS_STATUS_ERROR:
@@ -131,8 +137,42 @@ class WebsocketificationClient {
 		});
 	}
 
-	close() {
-		this.mWS.close();
+	/**
+	 * Set on broadcast listener.
+	 *
+	 * @param id{String} Broadcast id
+	 * @param callback{Function} Callback.
+	 */
+	setOnBroadcastListener(id, callback) {
+		this.mGlobalListeners[getID(METHOD_BROADCAST, id)] = callback;
+	}
+
+	/**
+	 * Set unhandled listener.
+	 * @param callback{Function} Callback.
+	 */
+	setOnUnhandledResponseListener(callback) {
+		this.mUnhandledListener = callback;
+	}
+
+	/**
+	 * Set on closed listener.
+	 *
+	 * Pass null to remove listener.
+	 * @param callback Callback.
+	 */
+	setOnClosedListener(callback) {
+		this.mOnClose = callback;
+	}
+
+	/**
+	 * Set on error listener.
+	 *
+	 * Pass null to remove listener.
+	 * @param callback Callback.
+	 */
+	setOnErrorListener(callback) {
+		this.mOnError = callback;
 	}
 }
 
