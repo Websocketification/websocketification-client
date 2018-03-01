0fdd5c5951660c062bb0770698e48ffe5882cb46
Fisher <fisher.subs@gmail.com>
2018-03-01 22:52:37 +0800

    [src/WebsocketificationClient] Added auto reconnection support.

--- src/WebsocketificationClient.js
+++ src/WebsocketificationClient.js
@@ -1,14 +1,23 @@
 'use strict';
 
-const WS_STATUS_CONNECTING = 'CONNECTING';
-const WS_STATUS_CONNECTED = 'CONNECTED';
-const WS_STATUS_DISCONNECTED = 'DISCONNECTED';
-const WS_STATUS_ERROR = 'ERROR';
+// @see https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
+// @see https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
+
+const Response = require('./Response');
+
+const WS_STATUS_CONNECTING = 'CONNECTING'; // = WebSocket.CONNECTING
+const WS_STATUS_CONNECTED = 'CONNECTED'; // = WebSocket.OPEN
+const WS_STATUS_DISCONNECTED = 'DISCONNECTED'; // = WebSocket.CLOSING || WebSocket.CLOSED
+const WS_STATUS_ERROR = 'ERROR'; // = WebSocket.CLOSING || WebSocket.CLOSED
 
 const METHOD_DEFAULT = 'GET';
 
 class WebsocketificationClient {
-	constructor(address) {
+	constructor(address, options = {
+		enableLogging: true,
+		retryWaitingTimeStart: 0,
+		retryWaitingTimeStep: 150,
+	}) {
 		[
 			'connect', 'close',
 			'onResponse',
@@ -17,7 +26,8 @@ class WebsocketificationClient {
 			'setOnClosedListener', 'setOnErrorListener'
 		].map(method => this[method] = this[method].bind(this));
 		this.fetch = this.fetch.bind(this);
-		this.mStatus = WS_STATUS_CONNECTING;
+		// The initial status is disconnected.
+		this.mStatus = WS_STATUS_DISCONNECTED;
 		this.mAddress = address;
 		/**
 		 * Temp listener that will be triggered only for once.
@@ -31,27 +41,82 @@ class WebsocketificationClient {
 		 * Unhandled listener that will be called for unhandled response.
 		 */
 		this.mUnhandledListener = undefined;
+		this.mEnableLogging = options.enableLogging;
+		if (this.mEnableLogging) {
+			// The default WebSocket.onerror event listener.
+			this.mOnError = (event) => console.error('WebSocket onerror callback triggered: ws.onerror(event)->', event);
+			// The default WebSocket.onclose event listener.
+			this.mOnClose = null;
+		}
+
+		// Waiting time for retry.
+		this.mRetryWaitingTimeStart = options.retryWaitingTimeStart;
+		this.mRetryWaitingTimeStep = options.retryWaitingTimeStep;
+		this.mRetryWaitingTime = this.mRetryWaitingTimeStart;
+		// Waiting time for CONNECTING => CONNECTED.
+		this.mRequestWaitingTimeStart = 100;
+		this.mRequestWaitingTimeStep = 20;
+	}
+
+	log(...args) {
+		if (this.mEnableLogging) {
+			console.log(...args);
+		}
 	}
 
 	connect() {
-		this.mWS = new WebSocket(this.mAddress);
-		this.mWS.onmessage = message => {
-			this.onResponse(JSON.parse(message.data));
-		};
 		return new Promise((resolve, reject) => {
-			this.mWS.onopen = () => {
+			try {
+				this.mWS = new WebSocket(this.mAddress);
+			} catch (ex) {
+				reject(ex, 'Invalid address: ' + address);
+				return
+			}
+			const ws = this.mWS;
+			this.log(`Connecting to ${ws.url}.`);
+			// The corresponding status of mWS which has more status than mWS.readyState.
+			this.mStatus = WS_STATUS_CONNECTING;
+			ws.onmessage = (message) => {
+				const response = Response.NewInstance(message.data);
+				if (!response) {
+					this.log('Unexpected response data:', message.data);
+					return;
+				}
+				if (!response.isValid()) {
+					this.log('Invalid response(fields of id and status are required):', response);
+					return;
+				}
+				this.onResponse(response);
+			};
+			ws.onopen = (event) => {
 				this.mStatus = WS_STATUS_CONNECTED;
-				resolve();
+				this.log(`Connected to ${ws.url}.`);
+				this.mRetryWaitingTime = this.mRetryWaitingTimeStart;
+				resolve(event);
 			};
-			this.mWS.onerror = error => {
-				this.mStatus = WS_STATUS_ERROR;
-				if (this.mOnError) {this.mOnError(error);}
-				reject();
+			ws.onerror = (event) => {
+				if (this.mOnError) {
+					this.mOnError(event);
+				}
+				reject(event);
 			};
-			this.mWS.onclose = () => {
-				this.mStatus = WS_STATUS_DISCONNECTED;
-				if (this.mOnClose) {this.mOnClose();}
-				reject();
+			ws.onclose = (event) => {
+				if (event.wasClean) {
+					// Connection is elegantly closed.
+					this.mStatus = WS_STATUS_DISCONNECTED;
+				} else {
+					this.mStatus = WS_STATUS_ERROR;
+					this.log(`WebSocket closed and waiting for ${this.mRetryWaitingTime} milliseconds before start a new connection.`);
+					// Retry connection.
+					setTimeout(() => {
+						this.connect();
+						this.mRetryWaitingTime += this.mRetryWaitingTimeStep;
+					}, this.mRetryWaitingTime);
+				}
+				if (this.mOnClose) {
+					this.mOnClose(event);
+				}
+				reject(event);
 			};
 		})
 	}
@@ -67,12 +132,10 @@ class WebsocketificationClient {
 	 * Handle response.
 	 */
 	onResponse(response) {
-		if (!response || !response.id || !response.status) {return;}
-		response.json = () => response.body;
 		let listener = this.mTempListeners[response.id];
 		if (listener) {
 			delete this.mTempListeners[response.id];
-			if (200 === response.status) {
+			if (response.isSuccess()) {
 				listener.resolve(response);
 			} else {
 				listener.reject(response);
@@ -81,7 +144,7 @@ class WebsocketificationClient {
 		}
 		listener = this.mGlobalListeners[response.id];
 		if (listener) {
-			if (200 === response.status) {
+			if (response.isSuccess()) {
 				listener(null, response);
 			} else {
 				listener(response);
@@ -103,9 +166,11 @@ class WebsocketificationClient {
 	fetch(path, options = {}) {
 		options.path = path;
 		options.id = `http$${Math.random()}@${+new Date()}`;
-		if (!options.method) {options.method = METHOD_DEFAULT;}
+		if (!options.method) {
+			options.method = METHOD_DEFAULT;
+		}
 		return new Promise((resolve, reject) => {
-			let time = 100;
+			let time = this.mRequestWaitingTimeStart;
 			let checkConnection = () => {
 				switch (this.mStatus) {
 					case WS_STATUS_CONNECTING:
@@ -113,7 +178,7 @@ class WebsocketificationClient {
 							checkConnection();
 						}, time);
 						// Slow down the loop.
-						time += 20;
+						time += this.mRequestWaitingTimeStep;
 						break;
 					case WS_STATUS_CONNECTED:
 						this.mWS.send(JSON.stringify(options));
@@ -121,6 +186,19 @@ class WebsocketificationClient {
 						this.mTempListeners[options.id] = {resolve, reject};
 						break;
 					case WS_STATUS_DISCONNECTED:
+						// Reactive websocket.
+						this.connect().then(() => {
+							if (this.mStatus !== WS_STATUS_CONNECTED) {
+								// This is not going to happen.
+								throw new Error('WebSocket ' + this.mStatus);
+							}
+							this.mWS.send(JSON.stringify(options));
+							// Set listener.
+							this.mTempListeners[options.id] = {resolve, reject};
+						}).catch((ex) => {
+							reject(ex);
+						});
+						break;
 					case WS_STATUS_ERROR:
 						reject(new Error('WebSocket ' + this.mStatus));
 						break;
